371b2cf60da370d06984e3646c487778306a2990
GoodRabbit <141356782+AdamantiumRabbit@users.noreply.github.com>
2023-12-15 17:53:43 -0300

    alterações minimas
    nada que tenha valor de descrever

--- servidor/model.js
+++ servidor/model.js
@@ -11,14 +11,17 @@ var conexao = mysql.createConnection({
 // conexao.query('UPDATE [TABLE_NAME] SET [VARIAVEL = VALOR, VARIAVEL = VALOR] WHERE[condicao]')
 // conexao.query('SELECT [COLUNA] FROM [TABLE_NAME] ')
 
-async function insertUser(usuarios)
-conexao.connect()
-let campos = 'tb_user (nm_user, cpf_user, cidade_user, email_user, senha_user, sexo_user)'
-let isercao usuarios
-connection.query(`INSERT INTO ${campos} VALUES(${})`,
- (err, rows, fields)=> {
-    if (err) throw err;
+export async function insertUser(usuarios){
+  let campos = 'tb_user (nm_user, cpf_user, cidade_user, email_user, senha_user, sexo_user)'
+  let insercao = `${usuarios.nome},${usuarios.cpf},${usuarios.cidade},${usuarios.email},${usuarios.senha},${usuarios.sexo} `
+
+  conexao.connect()
+  connection.query(`INSERT INTO ${campos} VALUES(${insercao})`,
+    (err, rows, fields)=> {
+      if (err) throw err;
     
-  });
+    });
   
   connection.end();
+
+}
