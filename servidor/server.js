6b153f3f1c1aef20cde48097f97b92648634e84c
Renan <renanalmeida201612@gmail.com>
2023-12-16 23:24:45 -0300

    bodyParser adicionado

--- servidor/server.js
+++ servidor/server.js
@@ -1,12 +1,18 @@
 import express from 'express'
 import cors from 'cors'
 import rota from './rotas.js'
+import bodyParser from 'body-parser'
 const porta= 3000
 
 const run = express()
 try
-{run.use(cors())
-run.use(express.json())
+{
+run.use(cors())
+
+// para poder ser usado usando axios e insomnia (axios é mto bom ^_^)
+run.use(bodyParser.urlencoded({ extended: false }));
+run.use(bodyParser.json());
+
 run.use("/",rota)
 }
 catch(error){
